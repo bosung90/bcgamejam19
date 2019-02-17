2e738ccabbe957cbd9ece71f2394eb6f51953d2e
Eric Kim <bosung90@gmail.com>
2019-02-17 00:06:25 -0800

    render costumes

--- src/pages/Classroom/index.js
+++ src/pages/Classroom/index.js
@@ -54,9 +54,9 @@ export default function Classroom({ match }) {
             const studentsArr = Object.values(studentsData)
             return studentsArr.map((student, index) => {
               if (student.id === userId) {
-                return <MyCharacter key={student.id} skinId={student.skinId} />
+                return <MyCharacter key={student.id} {...student} />
               }
-              return <Character key={student.id} index={index} skinId={student.skinId} />
+              return <Character key={student.id} index={index} {...student} />
             })
           }}
         </Select>
@@ -67,19 +67,47 @@ export default function Classroom({ match }) {
 
 function MyCharacter(props) {
   const skinItem = dispatch.items.getItems()[props.skinId] || {}
+  const eyeItem = dispatch.items.getItems()[props.eyeId] || {}
+  const cheekItem = dispatch.items.getItems()[props.cheekId] || {}
+  const mouthItem = dispatch.items.getItems()[props.mouthId] || {}
+  const costumeItem = dispatch.items.getItems()[props.costumeId] || {}
+  const hairItem = dispatch.items.getItems()[props.hairId] || {}
   return (
     <View pointerEvents="none" absolute left={0} right={0} bottom={'33%'} alignCenter>
-      <img src={skinItem.imageUrl} alt="skin" style={{ width: 200 }} />
+      {!!skinItem.imageUrl && <img src={skinItem.imageUrl} alt="skin" style={{ width: 200 }} />}
+      {!!eyeItem.imageUrl && <img src={eyeItem.imageUrl} alt="eye" style={{ width: 200, position: 'absolute' }} />}
+      {!!cheekItem.imageUrl && (
+        <img src={cheekItem.imageUrl} alt="cheek" style={{ width: 200, position: 'absolute' }} />
+      )}
+      {!!mouthItem.imageUrl && (
+        <img src={mouthItem.imageUrl} alt="mouth" style={{ width: 200, position: 'absolute' }} />
+      )}
+      {!!costumeItem.imageUrl && (
+        <img src={costumeItem.imageUrl} alt="costume" style={{ width: 200, position: 'absolute' }} />
+      )}
+      {!!hairItem.imageUrl && <img src={hairItem.imageUrl} alt="hair" style={{ width: 200, position: 'absolute' }} />}
     </View>
   )
 }
 
 function Character(props) {
   const skinItem = dispatch.items.getItems()[props.skinId] || {}
+  const eyeItem = dispatch.items.getItems()[props.eyeId] || {}
+  const cheekItem = dispatch.items.getItems()[props.cheekId] || {}
+  const mouthItem = dispatch.items.getItems()[props.mouthId] || {}
+  const costumeItem = dispatch.items.getItems()[props.costumeId] || {}
+  const hairItem = dispatch.items.getItems()[props.hairId] || {}
 
   return (
     <View pointerEvents="none" absolute left={5 + 8 * (props.index % 8) + '%'} bottom={props.index < 9 ? '20%' : '5%'}>
-      <img src={skinItem.imageUrl} alt="skin" style={{ width: 90 }} />
+      {!!skinItem.imageUrl && <img src={skinItem.imageUrl} alt="skin" style={{ width: 90 }} />}
+      {!!eyeItem.imageUrl && <img src={eyeItem.imageUrl} alt="eye" style={{ width: 90, position: 'absolute' }} />}
+      {!!cheekItem.imageUrl && <img src={cheekItem.imageUrl} alt="cheek" style={{ width: 90, position: 'absolute' }} />}
+      {!!mouthItem.imageUrl && <img src={mouthItem.imageUrl} alt="mouth" style={{ width: 90, position: 'absolute' }} />}
+      {!!costumeItem.imageUrl && (
+        <img src={costumeItem.imageUrl} alt="costume" style={{ width: 90, position: 'absolute' }} />
+      )}
+      {!!hairItem.imageUrl && <img src={hairItem.imageUrl} alt="hair" style={{ width: 90, position: 'absolute' }} />}
     </View>
   )
 }
