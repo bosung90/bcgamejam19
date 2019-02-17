40a6373962bd9a775687111c2b5672ebb401f5b2
Eric Kim <bosung90@gmail.com>
2019-02-16 18:41:14 -0800

    you can now join class

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { auth } from 'firebase/config'
+import { View } from 'components'
 
 export default function Home() {
   const [loading, setLoading] = useState(true)
@@ -8,6 +9,8 @@ export default function Home() {
   useEffect(() => {
     return auth.onAuthStateChanged(user => {
       setLoading(false)
+      if (user) {
+      }
     })
   }, [])
 
@@ -17,21 +20,50 @@ export default function Home() {
         'loading...'
       ) : (
         <>
-          {auth.currentUser && (
-            <>
-              Welcome {auth.currentUser.email.split('@')[0]}
+          {auth.currentUser ? (
+            <View fill>
+              <View row>
+                Welcome {auth.currentUser.email.split('@')[0]}
+                <button
+                  onClick={() => {
+                    auth.signOut().then(() => {
+                      forceUpdate(Date.now().valueOf())
+                    })
+                  }}
+                >
+                  Logout
+                </button>
+              </View>
+
               <button
                 onClick={() => {
-                  auth.signOut().then(() => {
-                    forceUpdate(Date.now().valueOf())
+                  auth.currentUser.getIdToken().then(token => {
+                    console.log(token)
+
+                    // fetch('https://us-central1-bcgamejam19.cloudfunctions.net/joinClass', {
+                    fetch('http://localhost:5000/bcgamejam19/us-central1/joinClass', {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: token,
+                      },
+                      body: JSON.stringify({
+                        classCode: 'abcdefg',
+                      }),
+                    })
+                      .then(result => result.json())
+                      .then(json => {
+                        console.log(json.success)
+                      })
                   })
                 }}
               >
-                Logout
+                <View fontSize={70}>+</View>
               </button>
-            </>
+            </View>
+          ) : (
+            <Redirect to="/login" />
           )}
-          {!auth.currentUser && <Redirect to="/login" />}
         </>
       )}
     </div>
